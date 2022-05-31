e04953bef62d964489381ef4d3ba5f46d61f0a58
Marsboy02 <debin4748@naver.com>
2022-06-01 01:12:47 +0900

    feat: 패트랩 리스트 기능 추가

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import discord
 from data import Token
 from HandTrap import Urara
-import discord
 from discord.ext import commands
 
 
@@ -31,21 +31,24 @@ async def Commands(ctx):
     embed = discord.Embed(title="TG 하이퍼 라이브러리언 명렁어 목록", description="마스터 듀얼에 도움이 되는 명렁어들이 있습니다!", color=0x32A4FF)
     embed.set_thumbnail(url="https://uploads3.yugioh.com/card_images/3946/detail/5736.jpg?1385135416")
     embed.add_field(name="!안녕", value="TG 하이퍼 라이브러리언과 인사합니다", inline=False)
-    embed.add_field(name="!우라라", value="우라라 타이밍을 조회합니다.", inline=False)
-    embed.add_field(name="!티어덱", value="티어덱을 조회합니다.", inline=False)
+    embed.add_field(name="!패트랩", value="패트랩의 타이밍을 조회합니다.", inline=False)
+    embed.add_field(name="!티어덱", value="마스터듀얼 티어덱을 조회합니다.", inline=False)
     embed.set_footer(text="Summoned by 강형준#5876", icon_url="https://uploads3.yugioh.com/card_images/3946/detail/5736.jpg?1385135416")
 
     await ctx.send(embed=embed)
 
 
-@bot.command(name="우라라 목록")
+@bot.command(name="패트랩")
 async def UraraList(ctx):
-    await ctx.send(list(Urara.keys()))
+    string = '조회가 가능한 덱 리스트 :\n'
+    for k in Urara.keys():
+        string += k + ' '
+    await ctx.send(string)
 
 
-@bot.command(name="우라라")
-async def Urara(ctx, args):
-    await ctx.send(list(Urara[args]))
+# @bot.command(name="우라라")
+# async def UraraTiming(ctx, args):
+#     await ctx.send(list(Urara[args]))
 
 
 @bot.command(name="티어덱")
