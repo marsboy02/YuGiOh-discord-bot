0a196f8cf8680cf35f7976a9943594f697bf6a19
Marsboy02 <debin4748@naver.com>
2022-06-01 15:55:06 +0900

    feat: 우라라 타이밍 조회 기능 추가

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## 개발환경 설정
 ``` bash
-$ pip3 install python3
+$ pip3 install python3 // 파이썬 설치 
 
 $ pip3 install discord // Linux or Mac
 $ pip install -U discord // Window
--- main.py
+++ main.py
@@ -16,21 +16,21 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=None)
 
 
-@bot.command(name="안녕")
-async def Hello(ctx):
+@bot.command()
+async def 안녕(ctx):
     await ctx.send('{}님, 반갑습니다.'.format(ctx.author.mention))
 
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return None
+# @bot.event
+# async def on_message(message):
+#     if message.author.bot:
+#         return None
+#
+#     await bot.process_commands(message)
 
-    await bot.process_commands(message)
 
-
-@bot.command(name="명령어")
-async def Commands(ctx):
+@bot.command()
+async def 명령어(ctx):
     embed = discord.Embed(title="TG 하이퍼 라이브러리언 명렁어 목록", description="마스터 듀얼에 도움이 되는 명렁어들이 있습니다!", color=0x32A4FF)
     embed.set_thumbnail(url="https://uploads3.yugioh.com/card_images/3946/detail/5736.jpg?1385135416")
     embed.add_field(name="!안녕", value="TG 하이퍼 라이브러리언과 인사합니다", inline=False)
@@ -42,25 +42,41 @@ async def Commands(ctx):
     await ctx.send(embed=embed)
 
 
-@bot.command(name="패트랩")
-async def UraraList(ctx):
-    string = '조회가 가능한 덱 리스트 :\n'
+@bot.command()
+async def 패트랩(ctx):
+    string = '우라라 타이밍 조회가 가능한 덱 리스트 :\n'
     for k in Urara.keys():
         string += k + ' '
     await ctx.send(string)
 
 
-@bot.command(name="어드민")
-async def admin(ctx):
+@bot.command()
+async def 어드민(ctx):
     await ctx.send("서버의 어드민은 {} 입니다.".format(ctx.guild.owner))
 
-# @bot.command(name="우라라")
-# async def UraraTiming(ctx, args):
-#     await ctx.send(list(Urara[args]))
+
+@bot.command()
+async def 우라라(ctx, arg):
+
+    if not arg:
+        string = '조회하고 싶은 덱을 입력해 주세요!\n' \
+                  'ex)!우라라 스파이랄\n' \
+                  '해당 명령어는 \'마스터듀얼 갤러리\'의 \'마스터듀얼정보글\' 님의 게시글을 참고했습니다!'
+
+    else:
+        string = '여기 적혀져 있는 순위는 모든 상황에 국한된 것이 아닙니다!\n' \
+                 '상대 덱의 용병 카드나 패 필드/묘지/제외 존에 있는 카드의 영향을 무시한\n' \
+                 '보편적으로 효과가 있는 상황에서의 순위입니다! 그 점 참고 해 주세요!\n\n'
+
+        Urara_list = Urara.get(arg)
+        for i in range(len(Urara_list)):
+            string += str((i+1)) + ' 순위 : ' + Urara_list[i] + '\n'
+
+    await ctx.send(string)
 
 
-@bot.command(name="티어덱")
-async def Tier(ctx):
+@bot.command()
+async def 티어덱(ctx):
     await ctx.send("티어덱 조회가 끝났습니다.")
 
 bot.run(Token)
