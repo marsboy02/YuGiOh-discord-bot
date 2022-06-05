ba6a59332bcc3fb71610d02ef33fe02e1ac1561c
Marsboy02 <debin4748@naver.com>
2022-06-05 23:42:04 +0900

    fix: 마크다운 수정

--- functions.py
+++ functions.py
@@ -3,9 +3,9 @@ def backtick(string):
     return '```' + string + '```'
 
 
-#string을 Bold으로 반환
-def bold(string):
-    return '**' + string + '**'
+# #string을 Bold으로 반환
+# def bold(string):
+#     return '**' + string + '**'
 
 
 
