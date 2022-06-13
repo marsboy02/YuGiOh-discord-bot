5534ff9e05fada937d4a5de2aa478f4aef907401
Marsboy02 <marsboy0619@gmail.com>
2022-06-13 19:27:26 +0900

    feat: 셀레니움 크롤링 스키마 설계

--- Selenium.py
+++ Selenium.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 
-'''
-크롬드라이버 설치
-https://www.db.yugioh-card.com/yugiohdb/card_search.action URL
-키워드 입력 후 엔터 -> 검색 완료
-'''
+driver = webdriver.Chrome()
+url = 'http://google.com'
+driver.get(url)
