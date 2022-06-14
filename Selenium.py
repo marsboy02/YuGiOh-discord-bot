6df25eae11d21c9d35d58ab1139c58d164b9dbd9
Marsboy02 <marsboy0619@gmail.com>
2022-06-14 16:43:42 +0900

    fix: selenium 크롤링 스키마 설계

--- Selenium.py
+++ Selenium.py
@@ -1,5 +1,15 @@
+import selenium
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 
-driver = webdriver.Chrome()
-url = 'http://google.com'
-driver.get(url)
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+URL = 'https://naver.com'
+
+driver = webdriver.Chrome("/Users/admin/sources/YuGiOh-discord-bot/YuGiOh-discord-bot/chromedriver")
+driver.get(url=URL)
