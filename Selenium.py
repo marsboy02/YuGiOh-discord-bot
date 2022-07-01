ef1f65e15489eccf90f96a9429be445409b3cf31
Marsboy02 <marsboy0619@gmail.com>
2022-07-01 22:03:58 +0900

    fix: selenium 크롤링 설정

--- Selenium.py
+++ Selenium.py
@@ -20,5 +20,8 @@ search_box = driver.find_element_by_id('keyword')
 search_box.send_keys(input)
 search_box.send_keys(Keys.RETURN)
 
+# js에서 코드 긁어와야 함
+# 가져와야하는것 카드이름, 카드사진, 카드설명, 공격력,수비력
 title = driver.find_element_by_xpath('//span[@id="card_image_0_1"]').text
 print(title)
+
