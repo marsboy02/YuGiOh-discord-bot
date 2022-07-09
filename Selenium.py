f234c4941f6536b7ac37876b88c42c254d48f3cf
Marsboy02 <marsboy0619@gmail.com>
2022-07-09 15:40:08 +0900

    fix: 헤로쿠 크롤링 환경 설정

--- Selenium.py
+++ Selenium.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 URL = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action'
 
 def search(input):
-    driver = webdriver.Chrome("./chromedriver")
+    driver = webdriver.Chrome("https://github.com/heroku/heroku-buildpack-chromedriver")
     driver.get(url=URL)
 
     search_box = driver.find_element_by_id('keyword')
@@ -32,25 +32,3 @@ def search(input):
     string += '효과:' + driver.find_element_by_xpath("//dl[@class='flex_1']/dd[@class='box_card_text c_text flex_1']").text
 
     return string
-
-
-# input = 'SPYRAL-지니어스'
-
-# driver = webdriver.Chrome("./chromedriver")
-# driver.get(url=URL)
-#
-# time.sleep(1)
-# search_box = driver.find_element_by_id('keyword')
-# search_box.send_keys(input)
-# search_box.send_keys(Keys.RETURN)
-# time.sleep(2)
-#
-# print(driver.find_element_by_id('card_image_0_1').get_attribute("src"))
-# print(driver.find_element_by_id('card_image_0_1').get_attribute('alt'))
-# print(driver.find_element_by_class_name("card_info_species_and_other_item").text)
-# print(driver.find_element_by_class_name("box_card_attribute").text)
-# print(driver.find_element_by_class_name("atk_power").text)
-# print(driver.find_element_by_class_name("def_power").text)
-# time.sleep(3)
-# print(driver.find_element_by_xpath("//dl[@class='flex_1']/dd[@class='box_card_text c_text flex_1']").text)
-
